8fc10c587464274c8b75ad10a9c96146f477d799
John Wakley <jwakley74@gmail.com>
2021-04-02 17:46:55 -0400

    updated compile_shaders.sh

--- compile_shaders.bat
+++
@@ -1,4 +0,0 @@
-!!need to setup VULKAN_SDK env var:
-$VULKAN_SDK\Bin32\glslc.exe src\shaders\simple_shader.vert -o src\shaders\simple_shader.vert.spv
-$VULKAN_SDK\Bin32\glslc.exe src\shaders\simple_shader.frag -o src\shaders\simple_shader.frag.spv
-pause
--- compile_shaders.sh
+++ compile_shaders.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# /home/havoc/android_sdk/ndk/21.4.7075529/shader-tools/linux-x86_64/glslc ./src/shaders/simple_shader.vert -o ./src/shaders/simple_shader.vert.spv
-# /home/havoc/android_sdk/ndk/21.4.7075529/shader-tools/linux-x86_64/glslc ./src/shaders/simple_shader.frag -o ./src/shaders/simple_shader.frag.spv
+echo
+echo "./src/shaders/simple_shader.vert -> ./src/shaders/simple_shader.vert.spv"
+glslc ./src/shaders/simple_shader.vert -o ./src/shaders/simple_shader.vert.spv
+# $HOME/dev/vulkansdk-linux-x86_64-1.2.170.0/1.2.170.0/x86_64/bin/glslc ./src/shaders/simple_shader.vert -o ./src/shaders/simple_shader.vert.spv
 
-$HOME/dev/vulkansdk-linux-x86_64-1.2.170.0/1.2.170.0/x86_64/bin/glslc ./src/shaders/simple_shader.vert -o ./src/shaders/simple_shader.vert.spv
-$HOME/dev/vulkansdk-linux-x86_64-1.2.170.0/1.2.170.0/x86_64/bin/glslc ./src/shaders/simple_shader.frag -o ./src/shaders/simple_shader.frag.spv
+echo "./src/shaders/simple_shader.frag -> ./src/shaders/simple_shader.frag.spv"
+glslc ./src/shaders/simple_shader.frag -o ./src/shaders/simple_shader.frag.spv
+# $HOME/dev/vulkansdk-linux-x86_64-1.2.170.0/1.2.170.0/x86_64/bin/glslc ./src/shaders/simple_shader.frag -o ./src/shaders/simple_shader.frag.spv
+
+echo
+echo "done."
+echo
