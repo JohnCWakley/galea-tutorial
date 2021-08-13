77c3043d82f826e3273bfde146143a6e5c54e156
John Wakley <jwakley74@gmail.com>
2021-08-13 07:24:24 -0400

    diffuse shading

--- src/simple_render_system.cpp
+++ src/simple_render_system.cpp
@@ -15,7 +15,7 @@ namespace ve
     struct SimplePushConstantData
     {
         glm::mat4 transform{1.f};
-        alignas(16) glm::vec3 color;
+        glm::mat4 normalMatrix{1.f};
     };
 
     SimpleRenderSystem::SimpleRenderSystem(Device &device, VkRenderPass renderPass)
@@ -74,8 +74,9 @@ namespace ve
         for (auto &obj : gameObjects)
         {
             SimplePushConstantData push{};
-            push.color = obj.color;
-            push.transform = projectionView * obj.transform.mat4();
+            auto modelMatrix = obj.transform.mat4();
+            push.transform = projectionView * modelMatrix;
+            push.normalMatrix = obj.transform.normalMatrix();
 
             vkCmdPushConstants(
                 commandBuffer,
